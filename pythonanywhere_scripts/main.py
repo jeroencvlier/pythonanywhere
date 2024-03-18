16c7853476db1a998e7fd975e75a94cfe3e251f6
Jeroen van Lier <jeroencvlier@gmail.com>
2024-03-18 21:12:47 +0200

    arg parse update

--- pythonanywhere_scripts/main.py
+++ pythonanywhere_scripts/main.py
@@ -3,26 +3,25 @@ import sys
 import logging
 import argparse
 
-
 from pythonanywhere_scripts.local_rsync import local_rsync
 from pythonanywhere_scripts.s3_sync import aws_sync
 from pythonanywhere_scripts import telegram_bot
 
-
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
-
 
 def is_running_on_pythonanywhere():
     """Check if the script is running on PythonAnywhere."""
     return "PYTHONANYWHERE_DOMAIN" in os.environ
 
-
-def main(args):
+def main():
     """Run the appropriate sync function based on the environment."""
+    parser = argparse.ArgumentParser(description="Sync data and optionally test Telegram notifications.")
+    parser.add_argument("--telegram-test", action="store_true", help="Run a test for Telegram notifications.")
+    args = parser.parse_args()
+
     if args.telegram_test:
         logging.info("Running Telegram test.")
         telegram_bot.send_mess("Test message from sync script.")
-
     elif is_running_on_pythonanywhere():
         logging.info("Running on PythonAnywhere.")
         aws_sync()
@@ -32,16 +31,5 @@ def main(args):
 
     sys.exit(0)
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Sync data and optionally test Telegram notifications."
-    )
-    parser.add_argument(
-        "--telegram-test",
-        action="store_true",
-        help="Run a test for Telegram notifications.",
-    )
-    args = parser.parse_args()
-
-    main(args)
+    main()
